01b90de9983f791fb52d88788483741a5faaba54
Yuriy Dobryanskiy <juriydobryansky@gmail.com>
2024-07-26 02:45:53 +0300

    lesson-1

---
+++ lesson-1/task-1/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+
+const rootElement = document.querySelector('#root');
+
+const greetingElement = React.createElement(
+  'div',
+  { className: 'greeting' },
+  'Hello, React!'
+);
+
+ReactDOM.render(greetingElement, rootElement);
