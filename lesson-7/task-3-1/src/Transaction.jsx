fb60c9b41d5d7db5d7102d3ee8a50694abbceabb
Yuriy Dobryanskiy <juriydobryansky@gmail.com>
2024-08-11 07:03:17 +0300

    fix formating

--- lesson-7/task-3-1/src/Transaction.jsx
+++ lesson-7/task-3-1/src/Transaction.jsx
@@ -3,19 +3,16 @@ import React from 'react';
 import styles from './Transaction.module.scss';
 
 const Transaction = ({ from, to, amount, rate, time }) => {
-  const formattedAmount = new Intl.NumberFormat('en-GB').format(amount);
-  const formattedRate = new Intl.NumberFormat('en-GB').format(rate);
   const formattedDate = moment(time).format('DD MMM');
   const formattedTime = moment(time).format('HH:mm');
+  const formattedAmount = new Intl.NumberFormat('en-GB').format(amount);
 
   return (
     <li className={styles.transaction}>
       <span className={styles.transaction__date}>{formattedDate}</span>
       <span className={styles.transaction__time}>{formattedTime}</span>
-      <span className={styles.transaction__assets}>
-        {from} → {to}
-      </span>
-      <span className={styles.transaction__rate}>{formattedRate}</span>
+      <span className={styles.transaction__assets}>{`${from} → ${to}`}</span>
+      <span className={styles.transaction__rate}>{rate}</span>
       <span className={styles.transaction__amount}>{formattedAmount}</span>
     </li>
   );
