880b8a165a780fa715f61da7ee79f1a739b0f8fd
YDobryansky <168298265+YDobryansky@users.noreply.github.com>
2024-08-17 17:13:38 +0300

    Update BoilingVerdict.jsx

--- lesson-10/task-3/src/BoilingVerdict.jsx
+++ lesson-10/task-3/src/BoilingVerdict.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const BoilingVerdict = ({ celsius }) => {
+export const BoilingVerdict = ({ celsius }) => {
   return (
     <p>{celsius >= 100 ? 'The water would boil.' : 'The water would not boil.'}</p>
   );
 };
-
-export default BoilingVerdict;
