a1838cb24dbd4205e85ca2dc5812010de856d383
Yuriy Dobryanskiy <juriydobryansky@gmail.com>
2024-08-17 16:44:12 +0300

    del

--- lesson-10/task-3/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Calculator from './Calculator';
-
-const root = document.querySelector('#root');
-
-ReactDOM.render(<Calculator />, root);
