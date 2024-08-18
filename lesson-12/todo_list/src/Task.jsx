93de4359fd6595c447a225b228b27677888f26c1
Yuriy Dobryanskiy <juriydobryansky@gmail.com>
2024-08-19 00:42:33 +0300

    lesson-12

---
+++ lesson-12/todo_list/src/Task.jsx
@@ -0,0 +1,28 @@
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Task = ({ id, done, text, onChange, onDelete }) => {
+  return (
+    <li className={classNames('list-item', { 'list-item_done': done })}>
+      <input
+        type="checkbox"
+        className="list-item__checkbox "
+        defaultChecked={done}
+        onChange={() => onChange(id)}
+      />
+      <span className="list-item__text"> {text}</span>
+      <button className="list-item__delete-btn" onClick={() => onDelete(id)}></button>
+    </li>
+  );
+};
+
+export default Task;
+
+Task.propTypes = {
+  id: PropTypes.string,
+  done: PropTypes.bool,
+  text: PropTypes.string,
+  onChange: PropTypes.func,
+  onDelete: PropTypes.func,
+};
---
+++ lesson-12/todo_list/src/tasksGateway.js
@@ -0,0 +1,54 @@
+const baseUrl = 'https://666c261a49dbc5d7145cd879.mockapi.io/api/v1/tasks';
+
+export const createTask = taskData => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(taskData),
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to create task');
+    }
+  });
+};
+
+export const fetchTasksList = () => {
+  return fetch(baseUrl)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(taskList =>
+      taskList.map(({ _id, ...task }) => ({
+        id: _id,
+        ...task,
+      })),
+    );
+};
+
+export const updatedTasks = (taskId, taskData) => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(taskData),
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to update task ');
+    }
+  });
+};
+
+export const deleteTask = taskId => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'DELETE',
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to delete task');
+    }
+  });
+};
