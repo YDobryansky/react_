037ad5ea6d0d22cccd698a6a3b3646bdb54670ab
Yuriy Dobryanskiy <juriydobryansky@gmail.com>
2024-09-04 03:58:28 +0300

    mod

--- lesson-6/task-5/src/1/Auth.jsx
+++ lesson-6/task-5/src/1/Auth.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import Login from "./Login";
-import Logout from "./Logout";
-import Spinner from "./Spinner";
+import React, { useState } from "react"
+import Login from "./Login"
+import Logout from "./Logout"
+import Spinner from "./Spinner"
 
 const Auth = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -35,3 +35,60 @@ const Auth = () => {
 };
 
 export default Auth;
+
+
+// import React, { Component } from "react";
+// import Login from "./Login";
+// import Logout from "./Logout";
+// import Spinner from "./Spinner";
+
+// class Auth extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isLoggedIn: false,
+//       loading: false
+//     };
+//   }
+
+//   handleLogin = () => {
+//     this.setState({ loading: true });
+//     setTimeout(() => {
+//       this.setState({
+//         loading: false,
+//         isLoggedIn: true
+//       });
+//     }, 2000);
+//   };
+
+//   handleLogout = () => {
+//     this.setState({ isLoggedIn: false });
+//   };
+
+//   render() {
+//     const { loading, isLoggedIn } = this.state;
+
+//     if (loading) {
+//       return <Spinner size="50px" />;
+//     }
+
+//     return (
+//       <div>
+//         {isLoggedIn ? (
+//           <Logout onLogout={this.handleLogout} />
+//         ) : (
+//           <Login onLogin={this.handleLogin} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+// export default Auth;
+
+// // REACT APPROACH
+// // 1. Divide the UI into component +++
+// // header, footer, main(text, table(row), navigation)
+// // 2. build static version in React +++
+// // 3. Thing about props, state(where, which) +++
+// // 4. Write logic in JS/TS (80% JS vs. 20% React)
